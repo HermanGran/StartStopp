03c51aa6a950b3a7b7e83081fd0b3397718670a7
HermanGran <142888421+HermanGran@users.noreply.github.com>
2024-09-23 14:52:36 +0200

    Created test program

--- src/Sensor/Sensor.cpp
+++ src/Sensor/Sensor.cpp
@@ -1,3 +1,14 @@
 //
 // Created by Herman Hårstad Gran on 23/09/2024.
 //
+#include "Sensor/Sensor.hpp"
+
+Sensor::Sensor(int pin) : pin(pin) {}
+
+void Sensor::setup() {
+    pinMode(pin, INPUT);
+}
+
+int Sensor::readSensor() {
+    return analogRead(pin);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
+#include "Sensor/Sensor.hpp"
 
+Sensor sensor(22);
+int laser = 7;
+bool running = false;
 
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    sensor.setup();
+    pinMode(laser, OUTPUT);
 }
 
 void loop() {
-// write your code here
+    digitalWrite(laser, HIGH);
+    int reading = sensor.readSensor();
+
+    if (reading < 300) {
+        running = true;
+    } else {
+        running = false;
+    }
+    Serial.print("Running: ");
+    Serial.print(running);
+    Serial.print("       Value: ");
+    Serial.println(reading);
 }
