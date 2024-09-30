1c432371d030243e2a2dff103a0602e58a193028
Wato133 <Wato133@users.noreply.github.com>
2024-09-30 15:44:06 +0200

    Added lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 #include "Sensor/Sensor.hpp"
 #include "Timer/Timer.hpp"
+#include <LiquidCrystal.h>
 
 Sensor sensor(22);
 // Initializes timer class, values are thresholds for sensor, timer-start and timer-stop
 Timer timer(sensor, 150, 5000, 2000);
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() {
     Serial.begin(9600);
     sensor.setup();
+    lcd.begin(16,2);
 }
 
 void loop() {
@@ -20,4 +25,9 @@ void loop() {
     Serial.print(sensor.readSensor());
     Serial.print("      Time: ");
     Serial.println(timer.getTime());
+
+    lcd.setCursor(0, 1);
+    //lcd.clear();
+    lcd.print(timer.getTime());
+
 }
